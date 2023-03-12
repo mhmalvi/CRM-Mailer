27ee214f8749d8949f5bbcee2e717e75ccbae9fa
Habib <habib@quadque.digital>
2023-03-12 10:20:04 +0600

    latest

--- src/v1/controllers/Mail-controller.js
+++ src/v1/controllers/Mail-controller.js
@@ -112,5 +112,50 @@ payment_mail = async (req, res) => {
   }
 }
 
-module.exports = { sendMail, payment_mail };
+registration_mail = async (req, res) => {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "tanjibrubyat@gmail.com",
+      pass: "qaaqcvbjgnyrhgrr",
+    },
+  });
+
+  const email = req.body.email
+  const name = req.body.name;
+  const password = req.body.password;
+
+  let file = path.join(__dirname, "../../../views/registration_mail.ejs");
+
+  const data = await ejs.renderFile(file, {
+    // text,
+    // type,
+    // name,
+    // student_id,
+    // lead_id,
+    // response,
+    // payment,
+    email,
+    name,
+    password
+  });
+  const recipient = ["megatanjib@gmail.com"];
+  for (let i = 0; i < recipient.length; i++) {
+    // console.log(recipient[i])
+    info = await transporter.sendMail({
+      from: "tanjibrubyat@gmail.com",
+      to: recipient[i],
+      subject: "registration status",
+      html: data,
+    });
+  }
+
+  if (info) {
+    res.status(200).send("success");
+  } else {
+    res.status(400).send("failed");
+  }
+};
+
+module.exports = { sendMail, payment_mail, registration_mail };
 
--- src/v1/router/router.js
+++ src/v1/router/router.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
 // const MailController = require("../controllers/Mail-controller");
-const { sendMail, payment_mail } = require("../controllers/Mail-controller");
+const {
+  sendMail,
+  payment_mail,
+  registration_mail,
+} = require("../controllers/Mail-controller");
 
 // middleware that is specific to this router
 // router.use((req, res, next) => {
@@ -10,8 +14,9 @@ const { sendMail, payment_mail } = require("../controllers/Mail-controller");
 //   next();
 // });
 // define the home page route
-router.route("/send-mail").post(sendMail)
+router.post("/send-mail",sendMail)
 router.post("/send-payment-mail", payment_mail);
+router.post("/send-registration-mail", registration_mail);
 // // define the about route
 // router.get("/about", (req, res) => {
 //   res.send("About birds");
