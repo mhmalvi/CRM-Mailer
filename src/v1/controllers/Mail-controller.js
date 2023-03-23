c6e3be6fa4454b1f58d608ebf7dee52fce8afb8b
Habib <habib@quadque.digital>
2023-03-23 12:16:11 +0600

    latest

--- src/v1/controllers/Mail-controller.js
+++ src/v1/controllers/Mail-controller.js
@@ -63,7 +63,7 @@ sendMail = async (req, res) => {
   for (let i = 0; i < recipient.length; i++) {
     // console.log(recipient[i])
     info = await transporter.sendMail({
-      from: "yuanhuafung2021@gmail.com",
+      from: "tanjibrubyat@gmail.com",
       to: recipient[i],
       subject: req.body.subject,
       html: data,
